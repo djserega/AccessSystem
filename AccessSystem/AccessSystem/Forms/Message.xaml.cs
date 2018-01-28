67362f5426a2720e77548aeeaabcdb086d08cb1b
dj_serega <galyuk.sergey@gmail.com>
2018-01-28 14:31:00 +0200

    form messages

---
+++ AccessSystem/AccessSystem/Forms/Message.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AccessSystem.Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для Message.xaml
+    /// </summary>
+    public partial class Message : Window
+    {
+        internal string TextMessage { get; set; }
+        internal int Timer { get; set; }
+
+        public Message()
+        {
+            InitializeComponent();
+        }
+        public Message(string textMessage)
+        {
+            InitializeComponent();
+
+            TextMessage = textMessage;
+        }
+
+        public Message(string textMessage, int timer)
+        {
+            InitializeComponent();
+
+            TextMessage = textMessage;
+            Timer = timer;
+        }
+
+        private void WindowMessage_Loaded(object sender, RoutedEventArgs e)
+        {
+            TextBlockMessage.Text = TextMessage;
+
+            if (Timer > 0)
+                ButtonOK.Content += $" ({Timer} с.)";
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ButtonOK_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
