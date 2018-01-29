44f65e2bc4c554a8b20119940f1d1d93d5d82a2d
djserega <galyuk.sergey@gmail.com>
2018-01-29 11:35:12 +0200

    add class user

---
+++ AccessSystem/AccessSystem/Models/Object/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessSystem.Models.Object
+{
+    public class User : IBaseRefClassName
+    {
+        public string this[string columnName]
+        {
+            get
+            {
+                string error = "";
+
+                switch (columnName)
+                {
+                    case "Code":
+                        if (Code.ToString().Count() > 9)
+                            error = "Длина должна быть не больше 9 символов.";
+                        else
+                        {
+                            try
+                            {
+                            }
+                            catch (Exception)
+                            {
+
+                                throw;
+                            }
+                        }
+                        break;
+                }
+
+                return error;
+            }
+        }
+        public string Error { get; }
+
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string Comment { get; set; }
+        public bool Deletion { get; set; }
+        public TypeUser TypeUser { get; set; }
+    }
+}
