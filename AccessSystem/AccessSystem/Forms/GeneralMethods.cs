8d9bafddc7f7aecebaa41ca013031a9fed5933c3
dj_serega <galyuk.sergey@gmail.com>
2018-01-31 21:15:17 +0200

    add class GeneralMethods

---
+++ AccessSystem/AccessSystem/Forms/GeneralMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AccessSystem.Forms
+{
+    internal class GeneralMethods : Page
+    {
+        internal void SetBaseStyleDataGrid(ref DataGrid dataGrid)
+        {
+            Style styleColumns = (Style)FindResource("DataGridColumnHeaderBase");
+
+            for (int i = 0; i < dataGrid.Columns.Count; i++)
+            {
+                dataGrid.Columns[i].HeaderStyle = styleColumns;
+                dataGrid.Columns[i].Header = $"  {dataGrid.Columns[i].Header}  ";
+            }
+        }
+    }
+}
