3506704198c32aadeab0eb13c7305bbcb72e9465
dj_serega <galyuk.sergey@gmail.com>
2018-02-02 21:22:06 +0200

    new class requestrole

---
+++ AccessSystem/AccessSystem/Models/Object/RequestRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessSystem.Models.Object
+{
+    public class RequestRole : IRole
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string Synonym { get; set; }
+        public int Checked { get; set; }
+    }
+}
