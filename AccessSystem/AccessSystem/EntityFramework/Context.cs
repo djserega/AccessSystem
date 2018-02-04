040cbea68aba910cb8d395d2c2475f1fc56e0e2e
dj_serega <galyuk.sergey@gmail.com>
2018-02-04 16:11:42 +0200

    start work EF. Add class Context and Repository

---
+++ AccessSystem/AccessSystem/EntityFramework/Context.cs
@@ -0,0 +1,17 @@
+﻿using AccessSystem.Models.Object;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessSystem.EntityFramework
+{
+    public class Context : DbContext
+    {
+        public DbSet<Base> Base { get; set; }
+        public DbSet<Request> Reauest { get; set; }
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ AccessSystem/AccessSystem/EntityFramework/Repository.cs
@@ -0,0 +1,19 @@
+﻿using AccessSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessSystem.EntityFramework
+{
+    interface IRepository<T> where T : BaseRefClass
+    {
+        IEnumerable<T> GetAllObject();
+        T GetObject(int id);
+        bool Create(T item);
+        bool Update(T item);
+        bool Delete(T item);
+        bool Save(T item);
+    }
+}
